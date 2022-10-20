0e46f7501d4e7872c3d13c0f08cf7904261a9995
am199 <am199@DESKTOP-CRREGV5.mshome.net>
2022-10-20 14:07:48 +0200

    Practica Conexion en curso

--- DigimonMongoCRUD/src/main/java/module-info.java
+++ DigimonMongoCRUD/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.mycompany.digimonmongocrud {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires mongo.java.driver;
     opens com.mycompany.digimonmongocrud to javafx.fxml;
     exports com.mycompany.digimonmongocrud;
 }
